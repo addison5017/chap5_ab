084e9b1cb89f4b494b80db6f71f0efc665e11aec
addison5017 <63955619+addison5017@users.noreply.github.com>
2023-07-01 13:28:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(1000)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(1000)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(1000)
+    }
 })
